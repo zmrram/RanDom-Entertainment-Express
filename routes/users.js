dc6f104a51a493610f098c4677d4fde86d797033
Tin Huynh <zmrram@gmail.com>
2017-06-13 11:27:44 -0700

    add flash messages

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,7 @@ router.get('/register', function(req, res) {
 });
 
 router.post('/register', function(req, res) {
+    //Form validation using express validator middleware
     req.checkBody('name', 'Name is required!').notEmpty();
     req.checkBody('username', 'Username is required').notEmpty();
     req.checkBody('email', 'Email is required').isEmail();
@@ -22,35 +23,39 @@ router.post('/register', function(req, res) {
         var query = {
             username: req.body.username
         };
+        //search the database for the username to see if it already exist
         User.findOne(query, function(err, otherUser) {
             if (err) {
                 console.log(err);
             } else {
+                //if the username already exist, flash error message and re-render register page
                 if (otherUser) {
-                    req.flash({ 'error_msg': 'Username already exist!' });
-                    res.render('register');
+                    req.flash('error_msg', 'Username already exist!');
+                    res.redirect('register');
                 } else {
+                    //if not create a new user
                     var newUser = new User({
                         username: req.body.username,
                         password: req.body.password,
                         email: req.body.email,
                         name: req.body.name
                     });
-                    var bcrypt = require('bcryptjs');
+
+                    // user bcryptjs to hash the password ( async operation)
                     bcrypt.genSalt(10, function(err, salt) {
                         bcrypt.hash(req.body.password, salt, function(err, hash) {
                             newUser.password = hash;
+                            //save the new user to MongoDB running in localhost
                             newUser.save(function(err) {
                                 if (err) {
                                     console.log(err);
                                 } else {
-                                    req.flash({ 'success_msg': 'Account created!' });
-                                    res.render('login');
+                                    req.flash('success_msg', 'Account created!');
+                                    res.redirect('login');
                                 }
                             });
                         });
                     });
-
                 }
             }
         });
