c18302f55ccb6ab0a2bedc89f0265f1c15179638
Tin Huynh <zmrram@gmail.com>
2017-06-13 10:54:48 -0700

    validate register form

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,19 @@ router.get('/register', function(req, res) {
     res.render('register');
 });
 
+router.post('/register', function(req, res) {
+    req.checkBody('name', 'Name is required!').notEmpty();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('email', 'Email is required').isEmail();
+    req.checkBody('password', 'Password is required').notEmpty();
+    req.checkBody('password2', 'Password do not match').equals(req.body.password);
+
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('register', { errors: errors });
+    }
+});
+
 router.get('/login', function(req, res) {
     res.render('login');
 });
